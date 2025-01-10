124fdcd41f4e283e4bd1f925c0706469bcc8b882
nortveeb2408 <conv.19aivfiiyfe2h7@fleep.io>
2025-01-10 19:15:31 +0200

    esimene

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css';
+import Linn from './Linn';
+
+import piltPariis from './pildid/paris__7_by_joeclaffeyjr_dfgnddo.png';
+import piltLondon from './pildid/london_pictures_iii_by_jenisei_d1z6dkf.jpg';
+import piltBerliin from './pildid/evening_in_gendarmenmarkt_by_rikitza_d7mm0zy.png';
+
+function App() {
+  const pariis = {
+    nimi: "Pariis",
+    huvitav: "palju kunsti",
+    pilt: piltPariis
+  };
+
+ 
+  const london = {
+    nimi: "London",
+    huvitav: "punased bussid!!!",
+    pilt: piltLondon
+  };
+
+  const linnad = [
+    pariis,
+    london,
+    {
+      nimi: "Berliin",
+      huvitav: "väga hea curryvorst",
+      pilt: piltBerliin
+    },
+    {
+      nimi: "Tallinn",
+      huvitav: "kilukarbivaade"
+    },
+  ]
+  
+  return (
+    <div className="App">
+      <h1>Linnad</h1>
+      { 
+          linnad.map(linn =>  <Linn linn={linn} />)
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Linn.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function Linn({linn}) {
+    const [kylastusi, setKylastusi] = useState(0)
+
+    const vajutus = (event) => {
+        console.log('klikiti nupul ' + event.target.innerText)
+        setKylastusi(kylastusi + 1)
+        console.log(kylastusi);
+    }
+
+    return (
+        <div>
+            <h1>{linn.nimi}</h1>
+            <div>{linn.huvitav}</div>
+            <button onClick={vajutus} >Külasta linna {linn.nimi}</button>
+            <span id="k"> -- {kylastusi} -- </span>
+            <div className="imgWrapper">
+                <img src={linn.pilt} />
+            </div>
+        </div>
+    )
+}
+
+export default Linn
